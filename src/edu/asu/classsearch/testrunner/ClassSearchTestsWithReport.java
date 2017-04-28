fa6adeb3891f5bf021d0c562d860d5db3b9aa185
Ujjwal Dasu (Student) <udasu@asurite.asu.edu>
2017-04-28 09:06:40 -0700

    Reporting Update

--- src/edu/asu/classsearch/testrunner/ClassSearchTestsWithReport.java
+++ src/edu/asu/classsearch/testrunner/ClassSearchTestsWithReport.java
@@ -4,9 +4,11 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.*;
 @RunWith(Cucumber.class)
-@CucumberOptions(features={"src/edu/asu/classearch/features/test.feature","src/edu/asu/classearch/features/test.feature"},glue="edu.asu.classsearch.testcases.def",format = { "pretty","json:C:/Users/udasu/Dropbox (ASU)/automation/Reports/cucumber.json" })
+@CucumberOptions(features={"src/edu/asu/classearch/features/test.feature","src/edu/asu/classearch/features/test.feature"},
+								glue="edu.asu.classsearch.testcases.def",
+									format = { "pretty","json:C:/Users/udasu/Dropbox (ASU)/automation/Reports/cucumber.json" })
 public class ClassSearchTestsWithReport {
 	
-  }
+}
 
  
